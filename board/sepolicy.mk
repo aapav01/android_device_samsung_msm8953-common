4a2f8fcc7abb71cecb6993679c3b628b8de0c226
minz1 <emerytang@gmail.com>
2018-02-26 23:17:34 -0600

    board: sepolicy: include legacy qcom sepolicy

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,5 +1,6 @@
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     $(COMMON_PATH)/sepolicy
